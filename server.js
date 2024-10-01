032da0e64e9c6d81b26eb072d832eaa9ad6fde7c
Léo Flôr <leopbflor8@gmail.com>
2024-10-01 11:02:43 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (request, response) => {
+
+    console.log(request)
+
+    response.send('td certo')
+})
+
+app.get('/usuarios', (request, response) => {
+    response.send('tmj, eh isso')
+})
+
+app.listen(3000)
+
+app.put('/usuarios')
+app.delete('/usuarios')
